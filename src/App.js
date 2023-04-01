9a47feedec102ded0aeabfc26462c4b7bd6b5a91
Tasneem Durbass <rivieretasneem@hotmail.com>
2023-04-01 19:32:38 +0200

    [FEAT] Responsiveness de la page + [DOCS] modification gitignore

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className='recipe-cards'>
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
+
+      <div id='slider-hp'>
+
       </div>
+
+      <div id='trending-recipes'>
+        <h1 className='block-title'>Trending Recipes</h1>
+        <div className='recipe-cards'>
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+        </div>
+        <a className='block-link' href="#">View More Recipes</a>
+      </div>
+      
     </div>
 
   );
--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,20 @@ function Header() {
   return (
     <header>
       <div id='container'>
-        <div id='child1'><img alt='Logo menu' src='/assets/logoMenu.png' height='50'/></div>
+        <div id='child1'>
+          <a href='#'>
+            <img alt='Logo menu' src='/assets/logoMenu.png' height='50'/>
+          </a>
+        </div>
         <div id='child2'><img alt='main logo' src='/assets/logo.webp'/></div>
         <div id='child3'>
-          <img alt='Logo barre de recherche ' src='/assets/loupe.png' height='35'/>
-          <input type='text' size='30'/>
-          <img alt='Logo utilisateur' src='/assets/utilisateur.png' height='50'/>
+          <div className='search-bar'>
+            <img alt='Logo barre de recherche ' src='/assets/loupe.png' height='35'/>
+            <input type='text' size='30'/>
+          </div>
+          <div className='account-icon'>
+            <a href='#'><img alt='Logo utilisateur' src='/assets/utilisateur.png' height='50'/></a>
+          </div>
         </div>
 
       </div>
