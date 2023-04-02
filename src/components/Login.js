f0ea24887f8ef23325f6e493a2db99995c667304
Oussama Bouakaz <bkzoussama6@gmail.com>
2023-04-02 23:48:11 +0200

    regler probleme de responsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,6 +23,7 @@ import LockIcon from "@mui/icons-material/Lock";
 import image from "./brooke-lark-08bOYnH_r_E-unsplash.jpg";
 import logo from "./AWS Logo.png";
 import MenuIcon from "@mui/icons-material/Menu";
+import { createTheme } from "@mui/material/styles";
 import { positions } from "@mui/system";
 import { alpha, styled } from "@mui/material/styles";
 export default function login() {
@@ -44,9 +45,18 @@ export default function login() {
       },
     },
   });
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        mobile: 0,
+        tablet: 640,
+        laptop: 1024,
+        desktop: 1200,
+      },
+    },
+  });
   return (
     <Paper
-      container
       style={{
         backgroundImage: `url(${image})`,
         backgroundRepeat: "no-repeat",
@@ -70,140 +80,142 @@ export default function login() {
           </Grid>
         </Toolbar>
       </AppBar>
-      <Grid container direction="row" spacing={2}>
-        <Grid item sm={6} md={8} xs={12}>
-          <Card
-            sx={{
-              backgroundColor: "background.default",
-              flexDirection: "column",
-              height: "100%",
-              justifyContent: "center",
-              boxShadow: 10,
-              borderRadius: "3vh",
-            }}
-            elevation={20}
-            style={{
-              width: "50vh",
-              height: "70vh",
-              marginLeft: "120vh",
-              padding: 10,
-            }}
-          >
-            <Grid align="center">
-              <Avatar style={avatarStyle}>
-                <LockIcon></LockIcon>
-              </Avatar>
-              <h3>CONNEXION</h3>
-              <Typography variant="subtitle1" style={{ paddingBottom: "3vh" }}>
-                Utiliser votre compte
-                <br />
-                The Recipe Rendez-vous
-              </Typography>
-            </Grid>
-            <Formik
-              initialValues={{
-                username: "",
-                password: "",
+      <Container>
+        <Grid container justifyContent="flex-end">
+          <Grid item style={theme.breakpoints}>
+            <Card
+              sx={{
+                mt: 8,
+                backgroundColor: "background.default",
+                flexDirection: "column",
+                height: "100%",
+                justifyContent: "center",
+                boxShadow: 10,
+                borderRadius: "3vh",
               }}
-              validationSchema={Yup.object().shape({
-                username: Yup.string()
-                  .max(255)
-                  .required("username est obligatoire"),
-                password: Yup.string()
-                  .max(255)
-                  .required("password est obligatoire"),
-              })}
-              onSubmit={(values) => {}}
+              elevation={20}
+              style={{
+                width: "50vh",
+                height: "70vh",
+                padding: 10,
+              }}
             >
-              {({
-                errors,
-                handleBlur,
-                handleChange,
-                touched,
-                values,
-                setFieldValue,
-              }) => (
-                <Form>
-                  <Container>
-                    <CssBaseline />
-                    <Box
-                      sx={{
-                        marginTop: 2,
-
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "left",
-                      }}
-                    >
-                      <Divider />
+              <Grid align="center">
+                <Avatar style={avatarStyle}>
+                  <LockIcon></LockIcon>
+                </Avatar>
+                <h3>CONNEXION</h3>
+                <Typography
+                  variant="subtitle1"
+                  style={{ paddingBottom: "3vh" }}
+                >
+                  Utiliser votre compte
+                  <br />
+                  The Recipe Rendez-vous
+                </Typography>
+              </Grid>
+              <Formik
+                initialValues={{
+                  username: "",
+                  password: "",
+                }}
+                validationSchema={Yup.object().shape({
+                  username: Yup.string()
+                    .max(255)
+                    .required("username est obligatoire"),
+                  password: Yup.string()
+                    .max(255)
+                    .required("password est obligatoire"),
+                })}
+                onSubmit={(values) => {}}
+              >
+                {({
+                  errors,
+                  handleBlur,
+                  handleChange,
+                  touched,
+                  values,
+                  setFieldValue,
+                }) => (
+                  <Form>
+                    <Container>
+                      <CssBaseline />
                       <Box
                         sx={{
+                          marginTop: 2,
+
                           display: "flex",
                           flexDirection: "column",
-                          alignItems: "center",
+                          alignItems: "left",
                         }}
                       >
-                        <Grid container>
-                          <Grid item xs={12}>
-                            <CssTextField
-                              // className={styles.textfield}
-                              fullWidth
-                              error={Boolean(
-                                touched.username && errors.username
-                              )}
-                              helperText={touched.username && errors.username}
-                              onBlur={handleBlur}
-                              margin="normal"
-                              label="Nom d'utilisateur"
-                              id="username"
-                              name="username"
-                              value={values.username}
-                              onChange={handleChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <CssTextField
-                              fullWidth
-                              error={Boolean(
-                                touched.password && errors.password
-                              )}
-                              helperText={touched.password && errors.password}
-                              onBlur={handleBlur}
-                              margin="normal"
-                              name="password"
-                              type="password"
-                              label="Mot de passe"
-                              value={values.password}
-                              onChange={handleChange}
-                              id="password"
-                            />
-                          </Grid>
-                        </Grid>
-                        <Button
-                          type="submit"
-                          fullWidth
-                          variant="contained"
+                        <Divider />
+                        <Box
                           sx={{
-                            mt: 3,
-                            mb: 2,
-                            borderRadius: "20vh",
-                            background: "#f58220",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
                           }}
                         >
-                          Login
-                        </Button>
+                          <Grid container>
+                            <Grid item xs={12}>
+                              <CssTextField
+                                // className={styles.textfield}
+                                fullWidth
+                                error={Boolean(
+                                  touched.username && errors.username
+                                )}
+                                helperText={touched.username && errors.username}
+                                onBlur={handleBlur}
+                                margin="normal"
+                                label="Nom d'utilisateur"
+                                id="username"
+                                name="username"
+                                value={values.username}
+                                onChange={handleChange}
+                              />
+                            </Grid>
+                            <Grid item xs={12}>
+                              <CssTextField
+                                fullWidth
+                                error={Boolean(
+                                  touched.password && errors.password
+                                )}
+                                helperText={touched.password && errors.password}
+                                onBlur={handleBlur}
+                                margin="normal"
+                                name="password"
+                                type="password"
+                                label="Mot de passe"
+                                value={values.password}
+                                onChange={handleChange}
+                                id="password"
+                              />
+                            </Grid>
+                          </Grid>
+                          <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{
+                              mt: 3,
+                              mb: 2,
+                              borderRadius: "20vh",
+                              background: "#f58220",
+                            }}
+                          >
+                            Login
+                          </Button>
+                        </Box>
                       </Box>
-                    </Box>
-                  </Container>
-                </Form>
-              )}
-            </Formik>
-          </Card>
+                    </Container>
+                  </Form>
+                )}
+              </Formik>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item sm={6} md={8} xs={12}>
-          <h2>ouuuu</h2>
-        </Grid>
-      </Grid>
+      </Container>
     </Paper>
   );
 }
