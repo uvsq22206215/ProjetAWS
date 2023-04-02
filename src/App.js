bb02c0184ee57dba0a87d71a26f5aa663e459b38
Tasneem Durbass <rivieretasneem@hotmail.com>
2023-04-03 00:36:07 +0200

    [FEAT] Implementation du slider du homepage A noter: Faire npm install avant d'executer le code: Le dependency mdb react ui kit a ete utilise

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import './App.css';
 import './RecipeCard.css';
 import RecipeCard from './RecipeCard';
-import Header from './Header'
+import Header from './Header';
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <div id='slider-hp'>
+      <div id="slider-hp">
+        <MDBCarousel showIndicators showControls fade>
+          <MDBCarouselItem
+            className="w-100 d-block"
+            itemId={1}
+            src="/assets/slider1.jpg"
+            alt="..."
+          >
+            <h5 className='slider-title'>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </MDBCarouselItem>
 
+          <MDBCarouselItem
+            className="w-100 d-block"
+            itemId={2}
+            src="/assets/slider2.jpg"
+            alt="..."
+          >
+            {/* Pas besoin de mettre le contenu dans un div car un div(slider-caption) est automatiquement généré */}
+            <h5 className='slider-title'>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </MDBCarouselItem>
+
+          <MDBCarouselItem
+            className="w-100 d-block"
+            itemId={3}
+            src="/assets/slider3.jpg"
+            alt="..."
+          >
+            <h5 className='slider-title'>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </MDBCarouselItem>
+        </MDBCarousel>
       </div>
 
       <div id='trending-recipes'>
