157748df43dbd13b113f39cfd828f1f9f42db300
YounesJonas15 <younesamrane01@gmail.com>
2023-04-06 00:05:27 +0200

    update page de recherche

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,19 +1,17 @@
-
-function RecipeCard() {
+function RecipeCard(props) {
   return (
     <div className="recipe-card">
       <div className="card-image">
-        <img src="/assets/card-image.jpg" alt="card-image"/>
+        <img src={props.image} alt={props.title}/>
       </div>
       <div className="card-desc">
         <div>
-          <h1 className="card-title">Title</h1>
-          <p className="class-description">Lorem ipsum demet dolor...</p>
-          <a className="card-btn" href="#">View Recipe</a>
+          <h1 className="card-title">{props.title}</h1>
+          <p className="class-description">{props.description}</p>
+          <a className="card-btn" href={props.link}>View Recipe</a>
         </div>
       </div>
     </div>
   );
 }
-
 export default RecipeCard;
