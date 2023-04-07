6b6de468185bd0d8f78ecc2a51e82eff5d6a45bb
louis <louis200183@outlook.fr>
2023-04-07 14:42:17 +0200

    Correctifs erreur css RecipeCard.css SearchPage.css

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -16,59 +16,58 @@ function SearchPage() {
       <Header />
       <div id="search-container">
       <div id="filter-column">
-      <div class="filter">
-  <h2>Rechercher</h2>
-  <form>
-    <div class="form-group">
-      <label for="ingredients">Ingrédients :</label>
-      <input type="text" id="ingredients" name="ingredients" placeholder="Ajouter des ingrédients" />
-    </div>
-    <div class="form-group">
-      <label for="category">Catégorie :</label>
-      <select id="category" name="category">
-        <option value="">--Choisir une catégorie--</option>
-        <option value="entree">Entrée</option>
-        <option value="plat">Plat</option>
-        <option value="dessert">Dessert</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="time">Temps de préparation :</label>
-      <select id="time" name="time">
-        <option value="">--Choisir un temps--</option>
-        <option value="10">Moins de 10 minutes</option>
-        <option value="20">Moins de 20 minutes</option>
-        <option value="30">Moins de 30 minutes</option>
-        <option value="60">Moins de 1 heure</option>
-        <option value="120">Moins de 2 heures</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="difficulty">Niveau de difficulté :</label>
-      <select id="difficulty" name="difficulty">
-        <option value="">--Choisir un niveau--</option>
-        <option value="facile">Facile</option>
-        <option value="moyen">Moyen</option>
-        <option value="difficile">Difficile</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="culture">Culture :</label>
-      <select id="culture" name="culture">
-        <option value="">--Choisir une culture--</option>
-        <option value="francaise">Française</option>
-        <option value="italienne">Italienne</option>
-        <option value="asiatique">Asiatique</option>
-        <option value="mexicaine">Mexicaine</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <button type="submit">Rechercher</button>
-    </div>
-  </form>
-</div>
-
-</div>
+        <div class="filter">
+          <h2>Rechercher</h2>
+          <form>
+            <div class="form-group">
+              <label for="ingredients">Ingrédients :</label>
+              <input type="text" id="ingredients" name="ingredients" placeholder="Ajouter des ingrédients" />
+            </div>
+            <div class="form-group">
+              <label for="category">Catégorie :</label>
+              <select id="category" name="category">
+                <option value="">--Choisir une catégorie--</option>
+                <option value="entree">Entrée</option>
+                <option value="plat">Plat</option>
+                <option value="dessert">Dessert</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="time">Temps de préparation :</label>
+              <select id="time" name="time">
+                <option value="">--Choisir un temps--</option>
+                <option value="10">Moins de 10 minutes</option>
+                <option value="20">Moins de 20 minutes</option>
+                <option value="30">Moins de 30 minutes</option>
+                <option value="60">Moins de 1 heure</option>
+                <option value="120">Moins de 2 heures</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="difficulty">Niveau de difficulté :</label>
+              <select id="difficulty" name="difficulty">
+                <option value="">--Choisir un niveau--</option>
+                <option value="facile">Facile</option>
+                <option value="moyen">Moyen</option>
+                <option value="difficile">Difficile</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="culture">Culture :</label>
+              <select id="culture" name="culture">
+                <option value="">--Choisir une culture--</option>
+                <option value="francaise">Française</option>
+                <option value="italienne">Italienne</option>
+                <option value="asiatique">Asiatique</option>
+                <option value="mexicaine">Mexicaine</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <button type="submit">Rechercher</button>
+            </div>
+          </form>
+        </div>
+      </div>
         <div id="result-column">
           <div id="trending-recipes">
             <h1 className="block-title">Résultat recherche</h1>
