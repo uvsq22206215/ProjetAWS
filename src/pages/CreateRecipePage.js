7f61964fe9d67144542841ebe7091ddb34f09efd
louis <louis200183@outlook.fr>
2023-05-03 21:03:27 +0200

    Création de recette [manque champ image + check si utilisateur connecté]

--- src/pages/CreateRecipePage.js
+++ src/pages/CreateRecipePage.js
@@ -6,38 +6,98 @@ import { IconButton } from "@mui/material";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import RecipeCard from "../components/RecipeCard";
+import { UserAuth } from "../context/Usercontext";
+import { CoPresent } from "@mui/icons-material";
+
 
 function CreateRecipePage() {
 
-  
-  
-  // const pushRecipe = () => {
-    //   listeRecette.forEach(function(obj) {
-      //     addDoc(collection(database, "recipe"), obj).then(function(docRef) {
-        //       console.log("Document written with ID: ", docRef.id);
-        //     })
-        //     .catch(function(error) {
-          //         console.error("Error adding document: ", error);
-          //     });
-    //   });
-    // };
-    
-    const [instructions, setInstructions] = useState([""]);
-    
-    const removeInstruction = (index) => {
-      let newList = [...instructions];
-      newList.splice(index, 1);
-      setInstructions(newList);
-    };
-    
-    const handleChangeIns = (e, index) => {
-      let tmp = [...instructions];
-      tmp[index] = e.target.value;
-      setInstructions(tmp);
-    };
+  const { usr, user } = UserAuth();
 
-    const [listIngredients, setListIngredients] = useState([
-      { quantite: "", nomIngr: "" },
+
+  const pushRecipe = (recipe) => {
+    addDoc(collection(database, "recipe"), recipe).then(function (docRef) {
+      //console.log("Document written with ID: ", docRef.id);
+    })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+  };
+
+  const [category, setCategory] = useState("Plat principal");
+  const handleChangeSelectCategory = (e) => {
+    switch (e.target.value) {
+      case "platPrincipal":
+        setCategory("Plat principal");
+        break;
+      case "moyenne":
+        setCategory("Accompagnement");
+        break;
+      case "boisson":
+        setCategory("Boisson");
+        break;
+      case "confiserie":
+        setCategory("Confiserie");
+        break;
+      case "amuseGueule":
+        setCategory("Amuse-gueule");
+        break;
+      default:
+        break;
+    }
+  };
+
+  
+  const [cost, setCost] = useState("Bon marché");
+  const handleChangeSelectCost = (e) => {
+    switch (e.target.value) {
+      case "bonMarche":
+        setCost("Bon marché");
+        break;
+      case "moyen":
+        setCost("Moyen");
+        break;
+      case "assezCher":
+        setCost("Assez cher");
+      default:
+        break;
+    }
+  };
+  
+  const [difficulty, setDifficulty] = useState("Très facile");
+  const handleChangeSelectDifficulty = (e) => {
+    switch (e.target.value) {
+      case "tresFacile":
+        setDifficulty("Très facile");
+        break;
+      case "moyenne":
+        setDifficulty("Moyenne");
+        break;
+      case "difficile":
+        setDifficulty("Difficile");
+        break;
+      default:
+        break;
+    }
+  };
+
+
+  const [instructions, setInstructions] = useState([""]);
+
+  const removeInstruction = (index) => {
+    let newList = [...instructions];
+    newList.splice(index, 1);
+    setInstructions(newList);
+  };
+
+  const handleChangeIns = (e, index) => {
+    let tmp = [...instructions];
+    tmp[index] = e.target.value;
+    setInstructions(tmp);
+  };
+
+  const [listIngredients, setListIngredients] = useState([
+    { quantity: "", name : "" },
   ]);
 
   const removeIngr = (index) => {
@@ -45,23 +105,23 @@ function CreateRecipePage() {
     newList.splice(index, 1);
     setListIngredients(newList);
   };
-  
+
   const handleChangeIngr = (e, index) => {
     let tmp = [...listIngredients];
     tmp[index][e.target.name] = e.target.value;
     setListIngredients(tmp);
   };
-  
+
   const [name, setName] = useState();
   const [cookTime, setCookTime] = useState();
   const [prepTime, setPrepTime] = useState();
   const [nbPersonnes, setNbPersonnes] = useState();
-  
+
   const [codeError, setCodeError] = useState("");
 
   const handleCreation = () => {
     setCodeError("");
-    let ingrFiltered = listIngredients.filter((e) => e.nomIngr != "");
+    let ingrFiltered = listIngredients.filter((e) => e.name != "");
     let instFiltered = instructions.filter((e) => e != "");
     if (name == null) {
       setCodeError("La recette doit être nomée.");
@@ -87,9 +147,24 @@ function CreateRecipePage() {
       setCodeError("La recette doit contenir au moins un ingrédient.");
       return;
     }
-    // TODO : Push la recette dans la base de données
+    // TODO : check si l'utilisateur est connecté, generer les keywords
+    
+    listIngredients.map((ingr) => {ingr.quantity = parseInt(ingr.quantity)});
+    pushRecipe({
+      author: usr.username,
+      categorie : category,
+      cookTime : parseInt(cookTime),
+      cost : parseInt(cost),
+      difficulty : difficulty,
+      // TODO : image
+      name : name,
+      numberPersons : parseInt(nbPersonnes),
+      prepTime : parseInt(prepTime),
+      listIngredients : ingrFiltered,
+      recipeInstructions : instFiltered,
+      totalTime : parseInt(prepTime) + parseInt(cookTime)
+    })
   };
-
 
 
   return (
@@ -107,21 +182,21 @@ function CreateRecipePage() {
           <input
             placeholder="Nom de la recette"
             onChange={(e) => setName(e.target.value)}
-            />
+          />
         </div>
         <div className="createInput">
           <input
             type="number"
             placeholder="Temps de préparation (en minutes)"
             onChange={(e) => setPrepTime(e.target.value)}
-            />
+          />
         </div>
         <div className="createInput">
           <input
             type="number"
             placeholder="Temps de cuisson (en minutes)"
             onChange={(e) => setCookTime(e.target.value)}
-            />
+          />
         </div>
         <div id="listInstructions">
           {instructions.map((instruction, index) => (
@@ -153,18 +228,18 @@ function CreateRecipePage() {
           {listIngredients.map((ingredient, index) => (
             <div className="ingredientInput" key={"ingredient-" + index}>
               <input
-                name="quantite"
-                type="text"
+                name="quantity"
+                type="number"
                 placeholder="Quantité"
                 onChange={(e) => handleChangeIngr(e, index)}
-                value={listIngredients[index].quantite || ""}
+                value={listIngredients[index].quantity || ""}
               />
               <input
-                name="nomIngr"
+                name="name"
                 type="text"
                 placeholder="Ingrédient"
                 onChange={(e) => handleChangeIngr(e, index)}
-                value={listIngredients[index].nomIngr || ""}
+                value={listIngredients[index].name || ""}
               />
               {index ? (
                 <button
@@ -178,7 +253,7 @@ function CreateRecipePage() {
                   onClick={() =>
                     setListIngredients([
                       ...listIngredients,
-                      { quantité: "", nomIngr: "" },
+                      { quantity : "", name: "" },
                     ])
                   }
                 >
@@ -187,40 +262,40 @@ function CreateRecipePage() {
               )}
             </div>
           ))}
-        <div className="createSelectInput">
-          <label>Difficulté :</label>
-          <select name="difficultyOption" className="createOptions">
-            <option value="tresFacile">Très facile</option>
-            <option value="moyenne">Moyenne</option>
-            <option value="difficile">Difficile</option>
-          </select>
-        </div>
-        <div className="createSelectInput">
-          <label>Coût :</label>
-          <select name="costOption" className="createOptions">
-            <option value="bonMarche">Bon marché</option>
-            <option value="moyen">Moyen</option>
-            <option value="assezCher">Assez cher</option>
-          </select>
-        </div>
-        {/* Possibilité de rendre les catégories dynamiques ici */}
-        <div className="createSelectInput">
-          <label>Catégorie :</label>
-          <select name="categoryOption" className="createOptions">
-            <option value="platPrincipal">Plat principal</option>
-            <option value="accompagnement">Accompagnement</option>
-            <option value="boisson">Boisson</option>
-            <option value="co+nfiserie">Confiserie</option>
-            <option value="amuseGueule">Amuse-gueule</option>
-          </select>
-        </div>
-        {(codeError != "") && <div>{codeError}</div>}
-        <button
-          id="createRecipeButton"
-          onClick={handleCreation}
-        >
-          Créer la recette
-        </button>
+          <div className="createSelectInput">
+            <label>Difficulté :</label>
+            <select name="difficultyOption" className="createOptions" onChange={handleChangeSelectDifficulty}>
+              <option value="tresFacile">Très facile</option>
+              <option value="moyenne">Moyenne</option>
+              <option value="difficile">Difficile</option>
+            </select>
+          </div>
+          <div className="createSelectInput">
+            <label>Coût :</label> 
+            <select name="costOption" className="createOptions" onChange={handleChangeSelectCost}>
+              <option value="bonMarche">Bon marché</option>
+              <option value="moyen">Moyen</option>
+              <option value="assezCher">Assez cher</option>
+            </select>
+          </div>
+          {/* Possibilité de rendre les catégories dynamiques ici */}
+          <div className="createSelectInput">
+            <label>Catégorie :</label>
+            <select name="categoryOption" className="createOptions" onChange={handleChangeSelectCategory}>
+              <option value="platPrincipal">Plat principal</option>
+              <option value="accompagnement">Accompagnement</option>
+              <option value="boisson">Boisson</option>
+              <option value="confiserie">Confiserie</option>
+              <option value="amuseGueule">Amuse-gueule</option>
+            </select>
+          </div>
+          {(codeError != "") && <div>{codeError}</div>}
+          <button
+            id="createRecipeButton"
+            onClick={handleCreation}
+          >
+            Créer la recette
+          </button>
         </div>
       </div>
       <Footer />
