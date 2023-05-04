a9f324df17d1c63dc7fa87f3631e18b94dc22a02
louis <louis200183@outlook.fr>
2023-05-04 19:05:02 +0200

    Génération de recettes fonctionnelle

--- src/pages/RecipeGenerator.js
+++ src/pages/RecipeGenerator.js
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import RecipeCard from "../components/RecipeCard";
 import { useState } from "react";
-import { collection, query, limit, getDocs, where, addDoc } from "firebase/firestore";
+import { collection, query, getDocs, where } from "firebase/firestore";
 import { database } from "../utils/firebase";
 import "../assets/css/RecipeGenerator.css"
 
@@ -11,7 +11,7 @@ function RecipeGenerator() {
   const [inputs, setInputs] = useState(["", "", ""]);
 
   const handleInputChange = (index, input) => {
-    let tmp = inputs;
+    let tmp = [...inputs];
     tmp[index] = input;
     setInputs(tmp);
   };
@@ -19,28 +19,57 @@ function RecipeGenerator() {
 
   const [recipes, setRecipes] = useState([]);
 
-  const generateRecipes = () => {
-    let validInputs = inputs.filter((input) => input != "");
+  const intersectionRecipes = (recipes1, recipes2) => {
+    let tmp = [];
+    console.log("args :", recipes1.rec, recipes2.rec);
+    for (let i = 0; i < recipes1.rec.length; i++) {
+      for (let k = 0; k < recipes2.rec.length; k++) {
+        if ((recipes1.rec)[i].data().name === (recipes2.rec[k]).data().name) {
+          tmp.push(recipes1.rec[i]);
+        }
+      }
+    }
+    if (tmp.length === 0) {
+      console.log("intersection vide");
+    } else {
+      console.log("tmp :", tmp);
+    }
+
+    return {ingrNames : [...new Set([...recipes1.ingrNames, ...recipes2.ingrNames])], rec : [...tmp]};
+  };
+
+  const generateRecipes = async () => {
+    let validInputs = inputs.filter((input) => input !== "");
     
-    if (validInputs.length > 0) {
-      let q = query(collection(database, "recipe"), where("keywords", "array-contains-any", validInputs), limit(21));
-      let tmpRecipes = [];
-      getDocs(q)
+    const tmpRecipes = [];
+    for (let i = 0; i < validInputs.length; i++) {
+      tmpRecipes.push({ingrNames : [validInputs[i]], rec : []});
+      await getDocs(query(collection(database, "recipe"), where("keywords", "array-contains", validInputs[i])))
       .then((snapshot) => {
         snapshot.forEach((doc) => {
-          tmpRecipes.push(doc.data());
-          console.log(doc.data())
+          tmpRecipes[i].rec.push(doc);
         });
-        setRecipes(tmpRecipes);
-        console.log("Received from database", tmpRecipes);
       })
       .catch((error) => {
         console.error(error);
       });
-    } else {
-      // Erreur
-      console.error("Erreur");
     }
+
+    // tmpRecipes contient pour chaque ingrédient mis en input les résultats de la BD
+    // A nous de faire l'intersection
+    let result = null;
+    for (let i = 0; i < tmpRecipes.length; i++) {
+      if (result == null) {
+        result = tmpRecipes[0];
+      } else {
+        result = intersectionRecipes(result, tmpRecipes[i])
+      } 
+      console.log("resultat intersection", i, ":", result);
+    }
+    if (result != null) {
+      setRecipes(result);
+    }
+    //console.log("résultat intersection des 3 :", result);
   }
 
   return (
@@ -71,8 +100,8 @@ function RecipeGenerator() {
         </div>
         <button onClick={generateRecipes}>Générer recettes</button>
         <div className="recipe-cards">
-              {recipes.map((recipe) => (
-                <RecipeCard image={recipe.image} title={recipe.name} link={"/recipe?id=" + recipe.id} />
+              {recipes.rec && recipes.rec.map((recipe) => (
+                <RecipeCard image={recipe.data().image} title={recipe.data().name} link={"/recipe?id=" + recipe.id} />
               ))}
             </div>
         <Footer />
